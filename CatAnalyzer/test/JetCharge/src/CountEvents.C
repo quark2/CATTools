0a56d7d0ef5ac15a6db67e2ade59d76cfae180d3
Geonmo <ry840901@gmail.com>
2016-09-05 23:08:04 +0900

    Add C macro code.

---
+++ CatAnalyzer/test/JetCharge/src/CountEvents.C
@@ -0,0 +1,82 @@
+#include "TFile.h"
+#include "TTree.h"
+#include "TBranch.h"
+#include "TTreeReader.h"
+#include "TTreeReaderValue.h"
+
+void CountEvents(const char* filename="cmskin_quality.root")
+{
+  // Variables used to store the data
+  //Int_t totalSize = 0; // Sum of data size (in bytes) of all events
+
+  // open the file
+  TFile *f = TFile::Open(filename);
+  if (f == 0) {
+    // if we cannot open the file, print an error message and return immediatly
+    printf("Error: cannot open data file.\n");
+    return;
+  }
+  int xmax = 1000;
+  TProfile* h1 = new TProfile("isJet1_bjet_all","isbjet1_all",100,1,xmax);
+  TProfile* h2 = new TProfile("isJet2_bjet_all","isbjet1_all",100,1,xmax);
+  TProfile* h3 = new TProfile("isJet1or2_bjet_all","isbjet1_all",100,1,xmax);
+  TProfile* h4 = new TProfile("isJet1and2_bjet_all","isbjet1_all",100,1,xmax);
+  TProfile* h5 = new TProfile("isJet1_bjet_qcut","isbjet1_qcut",100,1,xmax);
+  TProfile* h6 = new TProfile("isJet2_bjet_qcut","isbjet1_qcut",100,1,xmax);
+  TProfile* h7 = new TProfile("isJet1or2_bjet_qcut","isbjet1_qcut",100,1,xmax);
+  TProfile* h8 = new TProfile("isJet1and2_bjet_qcut","isbjet1_qcut",100,1,xmax);
+
+  TH1F* h11   = new TH1F("Jet1-lepton pairing_all", "pair_j1l_all",11,-5,6);
+  TH1F* h11_1 = new TH1F("Jet2-lepton pairing_all", "pair_j2l_all",11,-5,6);
+  TH1F* h12   = new TH1F("Jet1-lepton pairing_qcut","pair_j1l_qcut",11,-5,6);
+  TH1F* h12_1 = new TH1F("Jet2-lepton pairing_qcut", "pair_j2l_qcut",11,-5,6);
+
+  TTreeReader myReader("qualityAna/KinSolverQuality", f);
+  TTreeReaderArray<int> bjet_partonPdgId_all(myReader, "bjet_partonPdgId_all");
+  TTreeReaderArray<int> bjet_partonPdgId_qcut(myReader, "bjet_partonPdgId_qcut");
+  TTreeReaderArray<int> lepton_charge(myReader, "lepton_charge");
+  TTreeReaderValue<Double_t> pair_quality_all(myReader,  "pair_quality_all.quality_all");
+  TTreeReaderValue<Double_t> pair_quality_qcut(myReader, "pair_quality_qcut.quality_qcut");
+
+  // Loop over all entries of the TTree or TChain.
+  while (myReader.Next()) {
+  double x = (*pair_quality_all)*1e5;
+    if ( abs(bjet_partonPdgId_all[0])==5 || abs(bjet_partonPdgId_all[1])==5 ) h3->Fill(x,1);
+    else h3->Fill(x,0);
+
+    if ( abs(bjet_partonPdgId_all[0])==5 && abs(bjet_partonPdgId_all[1])==5 ) h4->Fill(x,1);
+    else h4->Fill(x,0);
+
+    if ( abs(bjet_partonPdgId_qcut[0])==5 || abs(bjet_partonPdgId_qcut[1])==5 ) h7->Fill(x,1);
+    else h7->Fill(x,0);
+  
+    if ( abs(bjet_partonPdgId_qcut[0])==5 && abs(bjet_partonPdgId_qcut[1])==5 ) h8->Fill(x,1);
+    else h8->Fill(x,0);
+    
+    int pair_j1_all = bjet_partonPdgId_all[0]*lepton_charge[0];
+    int pair_j2_all = bjet_partonPdgId_all[1]*lepton_charge[1];
+    int pair_j1_qcut = bjet_partonPdgId_qcut[0]*lepton_charge[0];
+    int pair_j2_qcut = bjet_partonPdgId_qcut[1]*lepton_charge[1];
+  }
+  h3->SetLineColor(kRed);
+  h3->SetMarkerColor(kRed);
+  h4->SetLineColor(kRed);
+  h4->SetMarkerColor(kRed);
+
+  h7->SetLineColor(kBlue);
+  h7->SetMarkerColor(kBlue);
+  h8->SetLineColor(kBlue);
+  h8->SetMarkerColor(kBlue);
+
+  TCanvas* c1 = new TCanvas("comp_plot","compPlot");
+  h3->Draw();
+  h7->Draw("same");
+  c1->SaveAs("compare_or.png");
+    
+  TCanvas* c2 = new TCanvas("co2","comparison plot2");
+  h4->Draw();
+  h8->Draw("same");
+  c2->SaveAs("compare_and.png");
+
+}
+
