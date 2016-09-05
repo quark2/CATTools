ccab4937b9a5bb3cd734116413a2fcc0f3b50c9a
Geonmo <ry840901@gmail.com>
2016-09-05 23:21:52 +0900

    Remove event loop.

--- CatAnalyzer/test/JetCharge/quality2.py
+++ CatAnalyzer/test/JetCharge/quality2.py
@@ -7,16 +7,17 @@ gROOT.SetBatch(True)
 
 gStyle.SetPalette(1)
 
-def loopTree( tree ) :
-  h1 = getTH1("b Matched without qcut", [50,1,51],tree,"pair_quality_all*1e5",matched_or)
 
-  for event in tree :
-    pass
-    #print event.pair_quality_all*1e5
-
+xmax=1000
 
 def anaTree( tree ) :
   label="reco"
+
+  matched_jet1_all = "abs(bjet_partonPdgId_all[0])==5"
+  matched_jet1_qcut = "abs(bjet_partonPdgId_qcut[0])==5"
+  matched_jet2_all = "abs(bjet_partonPdgId_all[1])==5"
+  matched_jet2_qcut = "abs(bjet_partonPdgId_qcut[1])==5"
+
   matched_or_all = "abs(bjet_partonPdgId_all[0])==5 || abs(bjet_partonPdgId_all[1])==5"
   matched_or_qcut = "abs(bjet_partonPdgId_qcut[0])==5 || abs(bjet_partonPdgId_qcut[1])==5"
   matched_and_all = "abs(bjet_partonPdgId_all[0])==5 && abs(bjet_partonPdgId_all[1])==5"
@@ -25,21 +26,34 @@ def anaTree( tree ) :
   matched_first_all = "bjet_partonPdgId_all[0]*lepton_charge[0]"
   matched_first_qcut = "bjet_partonPdgId_qcut[0]*lepton_charge[0]"
   
+  pq_all_up = "pair_quality_all>0"
+  pq_qcut_up = "pair_quality_qcut>0"
+  
   c1 = makeCanvas("quality")
 
-  h1 = getTH1("Quality for all jets", [100,1,101],tree,"pair_quality_all*1e5","1")
-  h2 = getTH1("Quality for qcut jets",[100,1,101],tree,"pair_quality_qcut*1e5","1")
-  h3 = getTH2("Quality vs Quality",[[100,1,101],[100,1,101]],tree,"pair_quality_qcut*1e5:pair_quality_all*1e5","1")
 
-  h4 = getTH1("b Matched without qcut", [100,1,101],tree, "pair_quality_all*1e5",matched_or_all)
-  h5 = getTH1("Quality for qcut jets",  [100,1,101],tree,"pair_quality_qcut*1e5",matched_or_qcut)
 
-  h6 = getTH1("b Matched without qcut", [100,1,101],tree, "pair_quality_all*1e5",matched_and_all)
-  h7 = getTH1("Quality for qcut jets",  [100,1,101],tree,"pair_quality_qcut*1e5",matched_and_qcut)
+  h1 = getTH1("Quality for all jets", [100,1,xmax],tree,"pair_quality_all*1e5","pair_quality_all>0")
+  h2 = getTH1("Quality for qcut jets",[100,1,xmax],tree,"pair_quality_qcut*1e5","1")
+  h3 = getTH2("Quality vs Quality",[[100,1,xmax],[100,1,xmax]],tree,"pair_quality_qcut*1e5:pair_quality_all*1e5","1")
 
-  h8 = getTH1("b Matched without qcut", [3,-1,2],tree, "bjet_partonPdgId_all[0]/5*lepton_charge[0]","1")
-  h9 = getTH1("Quality for qcut jets",  [3,-1,2],tree, "bjet_partonPdgId_qcut[0]/5*lepton_charge[0]","1")
+  h4 = getTH1("b Matched without qcut", [100,1,xmax],tree, "pair_quality_all*1e5",matched_or_all)
+  h5 = getTH1("Quality for qcut jets",  [100,1,xmax],tree,"pair_quality_qcut*1e5",matched_or_qcut)
 
+  h6 = getTH1("b Matched without qcut", [100,1,xmax],tree, "pair_quality_all*1e5",matched_and_all)
+  h7 = getTH1("Quality for qcut jets",  [100,1,xmax],tree,"pair_quality_qcut*1e5",matched_and_qcut)
+
+  h8 = getTH1("j1_pdgId_lq_no_qcut", [10,-5,6],tree, "bjet_partonPdgId_all[0]*lepton_charge[0]" ,pq_all_up)
+  h9 = getTH1("j1_pdgId_lq_with_qcut",  [10,-5,6],tree, "bjet_partonPdgId_qcut[0]*lepton_charge[0]",pq_qcut_up)
+
+  h10 = getTH1("j2_pdgId_lq_no_qcut", [10,-5,6],tree, "bjet_partonPdgId_all[1]*lepton_charge[1]",pq_all_up)
+  h11 = getTH1("j2_pdgId_lq_with_qcut",  [10,-5,6],tree, "bjet_partonPdgId_qcut[1]*lepton_charge[1]",pq_qcut_up)
+
+  h12= getTH1("j1_pdgId_no_qcut", [10,-5,6],tree, "bjet_partonPdgId_all[0] ",pq_all_up)
+  h13= getTH1("j1_pdgId_with_qcut",  [10,-5,6],tree, "bjet_partonPdgId_qcut[0]",pq_qcut_up)
+
+  h14 = getTH1("j2_pdgId_no_qcut", [10,-5,6],tree, "bjet_partonPdgId_all[1] ",pq_all_up)
+  h15 = getTH1("j2_pdgId_with_qcut",  [10,-5,6],tree, "bjet_partonPdgId_qcut[1]",pq_qcut_up)
 
   h1.SetLineColor(ROOT.kRed)
   h1.SetMarkerColor(ROOT.kRed)
@@ -101,9 +115,65 @@ def anaTree( tree ) :
  
   h8.Draw() 
   h9.Draw("same")
-  c6.SaveAs("pdgId.png")
+  c6.SaveAs("pdgId_lepCharge_jet1.png")
 
+  c7 = makeCanvas("Norm for first jet")
+  h8.Scale( 1/h8.Integral(-10,10))
+  h9.Scale( 1/h9.Integral(-10,10))
+  h8.Draw()
+  h9.Draw("same")
+  c7.SaveAs("pdgId_lepCharge_jet1_norm.png")
 
+  c8 = makeCanvas("Eff for second jet_max")
+  h10.SetMarkerColor(ROOT.kRed)
+  h10.SetLineColor(ROOT.kRed)
+  h11.SetMarkerColor(ROOT.kBlue)
+  h11.SetLineColor(ROOT.kBlue)
+ 
+  h10.Draw() 
+  h11.Draw("same")
+  c8.SaveAs("pdgId_lepCharge_jet2.png")
+
+  c9 = makeCanvas("Norm for second jet")
+  h10.Scale( 1/h10.Integral(-10,10))
+  h11.Scale( 1/h11.Integral(-10,10))
+  h10.Draw()
+  h11.Draw("same")
+  c9.SaveAs("pdgId_lepCharge_jet2_norm.png")
+
+  c10 = makeCanvas("jet1_pdgId")
+  h12.SetMarkerColor(ROOT.kRed)
+  h12.SetLineColor(ROOT.kRed)
+  h13.SetMarkerColor(ROOT.kBlue)
+  h13.SetLineColor(ROOT.kBlue)
+ 
+  h12.Draw() 
+  h13.Draw("same")
+  c10.SaveAs("pdgId_jet1.png")
+
+  c11 = makeCanvas("Norm jet1_pdgId")
+  h12.Scale( 1/h12.Integral(-10,10))
+  h13.Scale( 1/h13.Integral(-10,10))
+  h12.Draw()
+  h13.Draw("same")
+  c11.SaveAs("pdgId_jet1_norm.png")
+
+  c12 = makeCanvas("jet2_pdgId")
+  h14.SetMarkerColor(ROOT.kRed)
+  h14.SetLineColor(ROOT.kRed)
+  h15.SetMarkerColor(ROOT.kBlue)
+  h15.SetLineColor(ROOT.kBlue)
+ 
+  h14.Draw() 
+  h15.Draw("same")
+  c12.SaveAs("pdgId_jet2.png")
+
+  c13 = makeCanvas("Norm jet2_pdgId")
+  h14.Scale( 1/h14.Integral(-10,10))
+  h15.Scale( 1/h15.Integral(-10,10))
+  h14.Draw()
+  h15.Draw("same")
+  c13.SaveAs("pdgId_jet2_norm.png")
 
   print h1.Integral(1,101)
   print h2.Integral(1,101)
@@ -119,4 +189,3 @@ if __name__ == "__main__" :
     file0 = TFile.Open(sys.argv[1])
     tree = file0.Get("qualityAna/KinSolverQuality")
     anaTree(tree)
-    #loopTree(tree)
