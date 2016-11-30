408b006617513f0cf641c852b641d208af797c4b
Geonmo <ry840901@gmail.com>
2016-11-30 16:15:12 +0900

    Reduce product space.

--- CatProducer/python/dstarProducer_cfi.py
+++ CatProducer/python/dstarProducer_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 catDstars = cms.EDProducer("CATDStarProducer",
   jetLabel = cms.InputTag("slimmedJets"),
   mcLabel  = cms.InputTag("prunedGenParticles"),
-  maxNumPFCand = cms.int32(5),
+  maxNumPFCand = cms.int32(9999),
   maxDeltaR = cms.double(0.2),
   d0MassCut = cms.double(0.5),
   d0MassWindow = cms.double(0.05),
